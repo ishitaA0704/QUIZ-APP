6a44a9e345d437835bfedb565456a898027e3505
ishitaA0704 <ishitaagarwal3004@gmail.com>
2026-05-13 14:28:14 +0530

    Changes in stylign

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const [question, nextbut, qdisplay, adisplay] =
 const options = document.querySelectorAll(".op");
 let questions = [], current = 0, score = 0;
 
-async function fetchQuestions() {
+async function fetchq() {
   const res = await fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple");
   questions = (await res.json()).results;
-  showQ();
+  showq();
 }
 
-function showQ() {
+function showq() {
   nextbut.style.display = "none";
   const q = questions[current];
-  qdisplay.innerHTML = `Question ${current + 1} of ${questions.length}`;
-  question.innerHTML = q.question;
+  qdisplay.innerHTML = `Question ${current + 1} of ${questions.length}<br><br>${q.question}`;
 
   const answers = [...q.incorrect_answers, q.correct_answer].sort(() => Math.random() - 0.5);
 
@@ -22,24 +21,25 @@ function showQ() {
     btn.disabled = false;
     btn.style.backgroundColor = "#8bb6e3";
     btn.onclick = () => {
-      options.forEach(b => {
-        b.disabled = true;
-        if (b.innerHTML === q.correct_answer) b.style.backgroundColor = "green";
+      options.forEach(btn => {
+        btn.disabled = true;
+        if (btn.innerHTML === q.correct_answer) btn.style.backgroundColor = "green";
       });
-      if (btn.innerHTML !== q.correct_answer) { btn.style.backgroundColor = "red"; } else score++;
+      if (btn.innerHTML !== q.correct_answer) { btn.style.backgroundColor = "red"; } 
+      else score++;
       nextbut.style.display = "block";
     };
   });
 }
 
 nextbut.addEventListener("click", () => 
-  ++current < questions.length ? showQ() : showScore()
+  ++current < questions.length ? showq() : showscore()
 );
 
-function showScore() {
+function showscore() {
   nextbut.style.display = "none";
-   adisplay.style.display = "none";
+  adisplay.style.display = "none";
   question.style.display = "none";
-   qdisplay.innerHTML = `Quiz Finished!<br><br>Your Score: ${score} / ${questions.length}`;
+  qdisplay.innerHTML = `Quiz Finished!<br><br>Your Score: ${score} / ${questions.length}`;
 }
-fetchQuestions();
+fetchq();
